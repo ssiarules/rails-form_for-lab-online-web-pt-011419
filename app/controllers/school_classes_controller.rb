49f9b813a8b707e3e5f1755da5eff824c244fa5a
Johnson Joseph <jhonsonj88@gmail.com>
2019-06-19 23:33:21 +0000

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,17 @@
+class SchoolClassesController < ApplicationController
+  def new
+    #@school_classes = School_Classes.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+
+  resources :school_classes, only: [:index, :show, :new, :create, :edit, :update]
+
+  resources :students, only: [:index, :show, :new, :create, :edit, :update]
+
+  #get 'school_classes/new'
+
+  #get 'school_classes/create'
+
+  get 'school_classes/show'
+
+  get 'school_classes/edit'
+
+  get 'school_classes/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+#resources :posts, only: [:index, :show, :new, :create, :edit, :update]
